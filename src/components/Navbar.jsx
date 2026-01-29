2422d515e0e56df6167d432a73de62427d0c7b08
arjcc <25b054@nssce.ac.in>
2026-01-29 22:08:57 +0530

    vercell push failure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,9 +11,9 @@ const toLinkList = arr => (
 const Navbar = () => {
     return (
         <nav className='flex items-center justify-between px-10 py-5'>
-            <a className='flex' href='/'>
-                <img src="assets/achu.svg" alt="logo" />
-                <span>arjunliji</span>
+            <a className='flex items-center gap-1' href='/'>
+                <img src="../../public/favicon.ico" alt="logo" className='h-6' />
+                <span>Arjun Liji</span>
             </a>
             <ul className='hidden sm:flex gap-5'>{toLinkList(navLinks)}</ul>
             <a href="contact">Contact</a>
